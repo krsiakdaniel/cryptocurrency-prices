b977d9de4f01367a25b0f22c2eb0c5fd9b622df4
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-22 17:38:11 +0100

    ref: hero crypto images

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Heading, VStack, useColorModeValue } from '@chakra-ui/react'
+import { Box, Text, Heading, Image, Stack, VStack, useColorModeValue } from '@chakra-ui/react'
 import { Logo } from '../Logo'
 
 export const Hero = () => {
@@ -17,6 +17,29 @@ export const Hero = () => {
       <Text as="h2" mt={4} fontSize="xl">
         Display current cryptocurrency prices!
       </Text>
+
+      <Stack direction="row" justify={{ base: 'center', md: 'space-between' }}>
+        <Image
+          boxSize="100px"
+          src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
+          alt="Bitcoin"
+        />
+        <Image
+          boxSize="100px"
+          src="https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628"
+          alt="Ethereum"
+        />
+        <Image
+          boxSize="100px"
+          src="https://assets.coingecko.com/coins/images/325/large/Tether.png?1696501661"
+          alt="Tether"
+        />
+        <Image
+          boxSize="100px"
+          src="https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970"
+          alt="BNB"
+        />
+      </Stack>
 
       <VStack spacing={8} mt={8}>
         <Logo h="40vmin" pointerEvents="none" />
