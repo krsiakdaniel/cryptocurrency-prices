42c1dbe4b7e378801b4910e6d30e89e15d266a72
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-22 17:36:34 +0100

    feat: page layout widths

--- src/layout/PageContent/index.tsx
+++ src/layout/PageContent/index.tsx
@@ -4,9 +4,30 @@ type Props = {
   children: React.ReactNode
 }
 
+const pageWidth = {
+  base: '0px',
+  sm: '480px',
+  md: '768px',
+  lg: '992px',
+  xl: '1280px',
+  '2xl': '1536px',
+}
+
 export const PageContent = ({ children }: Props) => {
   return (
-    <Box textAlign="center">
+    <Box
+      mx="auto"
+      textAlign="center"
+      px={8}
+      maxWidth={{
+        base: '100%',
+        sm: pageWidth.sm,
+        md: pageWidth.md,
+        lg: pageWidth.lg,
+        xl: pageWidth.xl,
+        '2xl': pageWidth['2xl'],
+      }}
+    >
       <Grid minH="100vh" p={4} pt={10} bg={useColorModeValue('gray.100', 'gray.800')}>
         {children}
       </Grid>
