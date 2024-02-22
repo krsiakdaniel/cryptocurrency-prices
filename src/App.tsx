f351f01003f10dced5268b5b84dfecc10ef07caf
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-22 15:34:24 +0100

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
 import { ChakraProvider, Box, Text, Heading, Link, VStack, Grid, theme } from '@chakra-ui/react'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
-import { Logo } from './components/logo'
+
+import { Logo } from './components/Logo'
+import { Footer } from './layout/Footer'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3} bg="green.300">
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <Heading as="h1" size="4xl" noOfLines={1}>
-          CryptoMania
-        </Heading>
-        <Text as="h2" noOfLines={1}>
-          Display current cryptocurrency prices!
-        </Text>
-
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Using{' '}
-            <Link color="blue.600" href="https://www.coingecko.com/" target="_blank" rel="noopener noreferrer">
-              CoinGecko API
-            </Link>
+    <>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3} bg="gray.100">
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Heading as="h1" size="4xl" noOfLines={1}>
+            CryptoMania
+          </Heading>
+          <Text as="h2" noOfLines={1}>
+            Display current cryptocurrency prices!
           </Text>
-        </VStack>
-      </Grid>
-    </Box>
+
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Using{' '}
+              <Link color="blue.600" href="https://www.coingecko.com/" target="_blank" rel="noopener noreferrer">
+                CoinGecko API
+              </Link>
+            </Text>
+          </VStack>
+        </Grid>
+      </Box>
+      <Footer />
+    </>
   </ChakraProvider>
 )
