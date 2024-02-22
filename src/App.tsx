7d8c1a3514ac32b5cfe881c15adb86524fbb93a1
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-22 15:23:30 +0100

    ref: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ChakraProvider, Box, Text, Heading, Link, VStack, Grid, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+import { Logo } from './components/logo'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid minH="100vh" p={3} bg="green.300">
         <ColorModeSwitcher justifySelf="flex-end" />
+        <Heading as="h1" size="4xl" noOfLines={1}>
+          CryptoMania
+        </Heading>
+        <Text as="h2" noOfLines={1}>
+          Display current cryptocurrency prices!
+        </Text>
+
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Using{' '}
+            <Link color="blue.600" href="https://www.coingecko.com/" target="_blank" rel="noopener noreferrer">
+              CoinGecko API
+            </Link>
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
         </VStack>
       </Grid>
     </Box>
