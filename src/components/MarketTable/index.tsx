682e3cc660886d5f2088f11318f0e05685260f88
Krsiak Daniel <krsiak.daniel@gmail.com>
2024-02-24 13:29:24 +0100

    feat: add search

--- src/components/MarketTable/index.tsx
+++ src/components/MarketTable/index.tsx
@@ -4,6 +4,7 @@ import {
   Table,
   Heading,
   Image,
+  Input,
   Thead,
   Tbody,
   Tr,
@@ -26,12 +27,21 @@ import { getUrlAPI } from '../../utils/getUrlAPI'
 const API_PAGE_LIMIT = 500
 const ITEMS_PER_PAGE = 50
 
+const NoSearchResults = () => {
+  return (
+    <Box textAlign="center" mt={4}>
+      No results found
+    </Box>
+  )
+}
+
 export const MarketTable = () => {
   const urlCoins = getUrlAPI(API_PAGE_LIMIT)
 
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(1)
+  const [searchTerm, setSearchTerm] = useState('')
 
   const pages = Math.ceil(coins.length / ITEMS_PER_PAGE)
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE
@@ -41,6 +51,16 @@ export const MarketTable = () => {
   const handlePageChange = (page: number) => {
     setCurrentPage(page)
   }
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value)
+  }
+
+  // Filter coins based on search term
+  const filteredCoins = coinsOnCurrentPage.filter((coin: CoinTypeMarket) =>
+    coin.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
+  const noSearchResults = filteredCoins.length === 0
 
   useEffect(() => {
     fetch(urlCoins, {
@@ -67,108 +87,116 @@ export const MarketTable = () => {
   const bgButton = useColorModeValue('gray.200', 'gray.300')
   const activeButtonColor = useColorModeValue('green.400', 'pink.400')
 
+  if (loading) {
+    return (
+      <Flex width="full" mb={8} minH="275px" bgColor={loadingColors} alignItems="center" justifyContent="center">
+        <Text fontSize="lg">⏳ Loading table...</Text>
+      </Flex>
+    )
+  }
+
   return (
     <Box my={40}>
-      {loading ? (
-        <Flex width="full" mb={8} minH="275px" bgColor={loadingColors} alignItems="center" justifyContent="center">
-          <Text fontSize="lg">⏳ Loading table...</Text>
-        </Flex>
-      ) : (
-        <Box>
-          <Flex
-            justifyContent="space-between"
-            alignItems="center"
-            py={2}
-            borderBottom="1px solid"
-            borderColor="gray.200"
-            flexDirection={{ base: 'column', sm: 'row' }}
-          >
-            <Flex>
-              <Heading textTransform="uppercase" size={{ base: 'md', sm: 'lg' }} pb={{ base: 2, sm: 0 }}>
-                Market Value
-              </Heading>
-            </Flex>
-            <ButtonGroup spacing="2">
-              {Array.from({ length: pages }, (_, i) => (
-                <Button
-                  key={i}
-                  onClick={() => handlePageChange(i + 1)}
-                  bg={i + 1 === currentPage ? activeButtonColor : bgButton}
-                  size="sm"
-                  color={i + 1 === currentPage ? 'white' : 'gray.600'}
-                  _hover={{
-                    color: 'white',
-                    bgColor: hoverColor,
-                  }}
-                >
-                  {i + 1}
-                </Button>
-              ))}
-            </ButtonGroup>
+      <Box>
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          py={2}
+          borderBottom="1px solid"
+          borderColor="gray.100"
+          flexDirection={{ base: 'column', sm: 'row' }}
+        >
+          <Flex>
+            <Heading textTransform="uppercase" size={{ base: 'md', sm: 'lg' }} pb={{ base: 2, sm: 0 }}>
+              Market Value
+            </Heading>
           </Flex>
+          <ButtonGroup spacing="2">
+            {Array.from({ length: pages }, (_, i) => (
+              <Button
+                key={i}
+                onClick={() => handlePageChange(i + 1)}
+                bg={i + 1 === currentPage ? activeButtonColor : bgButton}
+                size="sm"
+                color={i + 1 === currentPage ? 'white' : 'gray.600'}
+                _hover={{
+                  color: 'white',
+                  bgColor: hoverColor,
+                }}
+              >
+                {i + 1}
+              </Button>
+            ))}
+          </ButtonGroup>
+        </Flex>
 
-          <TableContainer>
-            <Table size="sm" variant="simple" bg={colorTableRow}>
-              <Thead>
-                <Tr bg={bgTrRow}>
-                  <Th fontSize="md" py={4} color={bgTrText}>
-                    Coin
-                  </Th>
-                  <Th fontSize="md" py={4} color={bgTrText} isNumeric>
-                    Price
-                  </Th>
-                  <Th fontSize="md" py={4} color={bgTrText} isNumeric>
-                    24h Change
-                  </Th>
-                  <Th fontSize="md" py={4} color={bgTrText} isNumeric>
-                    Market Cap
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {coinsOnCurrentPage &&
-                  coinsOnCurrentPage.map((coin: CoinTypeMarket) => {
-                    return (
-                      <Tr key={coin.id} _hover={{ background: colorTableRowHover }}>
-                        <Td width="200px" maxWidth="200px">
-                          <Flex alignItems="center">
-                            <Image boxSize="30px" mr={4} src={coin.image} alt={coin.name} />
-                            <Link
-                              href={`https://www.coingecko.com/en/coins/${coin.id}`}
-                              isExternal
-                              fontWeight="bold"
-                              color={linkColor}
-                              _hover={{
-                                textDecoration: 'underline',
-                                color: hoverColor,
-                              }}
-                            >
-                              {coin.name}
-                            </Link>
-                          </Flex>
-                        </Td>
-                        <Td width="200px" maxWidth="200px" isNumeric>
-                          ${numberSeparateCommas(coin.current_price)}
-                        </Td>
-                        <Td width="200px" maxWidth="200px" isNumeric fontWeight="bold">
-                          <Code
-                            colorScheme={coin.price_change_percentage_24h >= 0 ? 'green' : 'red'}
-                            children={`${coin.price_change_percentage_24h >= 0 ? '↑' : '↓'} ${coin.price_change_percentage_24h.toFixed(1)}%`}
-                            px={2}
-                            py={1}
-                          />
-                        </Td>
-                        <Td width="200px" maxWidth="200px" isNumeric>
-                          ${numberSeparateCommas(coin.market_cap)}
-                        </Td>
-                      </Tr>
-                    )
-                  })}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-      )}
+        <Flex py={4}>
+          <Input placeholder="Search for a coin" value={searchTerm} onChange={handleSearchChange} />
+        </Flex>
+
+        <TableContainer>
+          <Table size="sm" variant="simple" bg={colorTableRow}>
+            <Thead>
+              <Tr bg={bgTrRow}>
+                <Th fontSize="md" py={4} color={bgTrText}>
+                  Coin
+                </Th>
+                <Th fontSize="md" py={4} color={bgTrText} isNumeric>
+                  Price
+                </Th>
+                <Th fontSize="md" py={4} color={bgTrText} isNumeric>
+                  24h Change
+                </Th>
+                <Th fontSize="md" py={4} color={bgTrText} isNumeric>
+                  Market Cap
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {filteredCoins &&
+                filteredCoins.map((coin: CoinTypeMarket) => {
+                  return (
+                    <Tr key={coin.id} _hover={{ background: colorTableRowHover }}>
+                      <Td width="200px" maxWidth="200px">
+                        <Flex alignItems="center">
+                          <Image boxSize="30px" mr={4} src={coin.image} alt={coin.name} />
+                          <Link
+                            href={`https://www.coingecko.com/en/coins/${coin.id}`}
+                            isExternal
+                            fontWeight="bold"
+                            color={linkColor}
+                            _hover={{
+                              textDecoration: 'underline',
+                              color: hoverColor,
+                            }}
+                          >
+                            {coin.name}
+                          </Link>
+                        </Flex>
+                      </Td>
+                      <Td width="200px" maxWidth="200px" isNumeric>
+                        ${numberSeparateCommas(coin.current_price)}
+                      </Td>
+                      <Td width="200px" maxWidth="200px" isNumeric fontWeight="bold">
+                        <Code
+                          colorScheme={coin.price_change_percentage_24h >= 0 ? 'green' : 'red'}
+                          children={`${coin.price_change_percentage_24h >= 0 ? '↑' : '↓'} ${coin.price_change_percentage_24h.toFixed(1)}%`}
+                          px={2}
+                          py={1}
+                        />
+                      </Td>
+                      <Td width="200px" maxWidth="200px" isNumeric>
+                        ${numberSeparateCommas(coin.market_cap)}
+                      </Td>
+                    </Tr>
+                  )
+                })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+
+        {noSearchResults && <NoSearchResults />}
+      </Box>
     </Box>
   )
 }
